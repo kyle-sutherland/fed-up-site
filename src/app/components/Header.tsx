9149f2c48edb12a3f0c7fe76b52afe42bd53148a
kyle-sutherland <velveteen.rever@gmail.com>
2025-02-06 15:40:44 -0500

    form

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="text-center py-10 content-center">
       <div className={"content-center container pb-6"}>
-        <Image src={"/Referendum-Signature-Post-1024x1024.png"} alt={"Logo"} width={600} height={600} className={"mx-auto"} />
+        <Image src={"/Referendum-Signature-Post-1024x1024.png"} alt={"Logo"} width={500} height={500} className={"mx-auto"} />
       </div>
       <h1 className="text-4xl font-bold text-gray-900 mb-4">
         FedUp Concordia Campaign
